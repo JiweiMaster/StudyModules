c76854d25f19f26b4a28872342ad66d790db5e69
jiwei <jiwei@nrec.com>
2019-01-28 16:51:25 +0800

    login

---
+++ UserCenter/src/main/java/com/jiwei/usercenter/ui/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.jiwei.usercenter.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.jiwei.usercenter.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
